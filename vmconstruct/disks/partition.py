73e28d47e075466a0c53ba0ecf0f0741ce8f12d3
James Dingwall <james@dingwall.me.uk>
2014-07-13 08:29:58 +0100

    Rework the self._init() functions so they don't need to return a value. GPT writes secondary header in the correct location on the disk.

--- vmconstruct/disks/partition.py
+++ vmconstruct/disks/partition.py
@@ -5,10 +5,19 @@ from random import choice
 from sparse_list import SparseList
 
 
+# There really should be no need to change these unless trying to generate something unusual.  Not well
+# tested with non default values!
+MBR_SECTOR_SIZE = 512	# size for calculation when generating a mbr partition table
+GPT_SECTOR_SIZE = 512	# size for calculation when generating a guid partition table
+GPT_PTE_SIZE = 128	# size of a gpt partition entry (128 is usual)
+GPT_PTE_ENTS = 128	# number of entries in the gpt pte array (128 is usual)
+
+
 """
 TODO:
 
 fix initial errors with crc for gpt
+zero out disk identifier in mbr for gpt
 """
 
 class InvalidParitionNumber(Exception):
@@ -32,7 +41,7 @@ class _partition(metaclass=abc.ABCMeta):
         self._logger = logging.getLogger(self.__class__.__module__+"."+self.__class__.__name__)
         self._logger.debug("Building empty partition table")
         self._bootable = None
-        self._bytes = self._init()
+        self._init()
 
 
     @abc.abstractmethod
@@ -88,17 +97,15 @@ class msdos(_partition):
         self._epartitions = []
 
         # Binary representation
-        empty = bytearray(b"\0"*512)
+        self._pt = bytearray(b"\0"*512)
         # disk signature
-        empty[440] = choice(range(0, 256))
-        empty[441] = choice(range(0, 256))
-        empty[442] = choice(range(0, 256))
-        empty[443] = choice(range(0, 256))
+        self._pt[440] = choice(range(0, 256))
+        self._pt[441] = choice(range(0, 256))
+        self._pt[442] = choice(range(0, 256))
+        self._pt[443] = choice(range(0, 256))
         # boot signature
-        empty[510] = 0x55
-        empty[511] = 0xaa
-
-        return(empty)
+        self._pt[510] = 0x55
+        self._pt[511] = 0xaa
 
 
     def addPartition(self, index, sizemb, filesystem, bootable=None):
@@ -129,7 +136,7 @@ class msdos(_partition):
     def write(self, file):
         self._logger.debug("Writing msdos partition table to {f}".format(f=file))
         with open(file, "rb+") as fp:
-            fp.write(self._bytes)
+            fp.write(self._pt)
 
 
     def diskSize(self):
@@ -165,29 +172,29 @@ class msdos(_partition):
             if not sizemb:
                 self._logger.debug("Zeroing PTE for partition {p}".format(p=(index+1)))
                 for zero in range(0, 16):
-                    self._bytes[offset + zero] = 0
+                    self._pt[offset + zero] = 0
             else:
                 if index == self._bootable:
-                    self._bytes[offset + 0x00] = 0x80		# partition type (0x80 = bootable)
+                    self._pt[offset + 0x00] = 0x80		# partition type (0x80 = bootable)
                 else:
-                    self._bytes[offset + 0x00] = 0x00
+                    self._pt[offset + 0x00] = 0x00
 
-                self._bytes[offset + 0x01] = 254		# chs start address (indicate lba)
-                self._bytes[offset + 0x02] = 255
-                self._bytes[offset + 0x03] = 255
+                self._pt[offset + 0x01] = 254		# chs start address (indicate lba)
+                self._pt[offset + 0x02] = 255
+                self._pt[offset + 0x03] = 255
 
-                self._bytes[offset + 0x04] = filesystem		# fs type
+                self._pt[offset + 0x04] = filesystem		# fs type
 
-                self._bytes[offset + 0x05] = self._bytes[offset + 0x01]	# chs end address
-                self._bytes[offset + 0x06] = self._bytes[offset + 0x02]
-                self._bytes[offset + 0x07] = self._bytes[offset + 0x03]
+                self._pt[offset + 0x05] = self._pt[offset + 0x01]	# chs end address
+                self._pt[offset + 0x06] = self._pt[offset + 0x02]
+                self._pt[offset + 0x07] = self._pt[offset + 0x03]
 
                 if next_start > (2**32 - 1):
                     raise PartitionTooLarge("The start sector is greater than 2^32-1")
-                self._bytes[offset + 0x08] = (next_start & 0xff)
-                self._bytes[offset + 0x09] = (next_start >> 8) & 0xff
-                self._bytes[offset + 0x0a] = (next_start >> 16) & 0xff
-                self._bytes[offset + 0x0b] = (next_start >> 24) & 0xff
+                self._pt[offset + 0x08] = (next_start & 0xff)
+                self._pt[offset + 0x09] = (next_start >> 8) & 0xff
+                self._pt[offset + 0x0a] = (next_start >> 16) & 0xff
+                self._pt[offset + 0x0b] = (next_start >> 24) & 0xff
 
                 # We'll work in 512 byte sectors but make sure we align to 4k byte
                 # (1048576 / 4096) * 8 = number of sectors
@@ -195,10 +202,10 @@ class msdos(_partition):
                 if sector_count > (2**32 - 1):
                     raise PartitionTooLarge("The partition size is more than 2^32-1 sectors")
 
-                self._bytes[offset + 0x0c] = sector_count & 0xff
-                self._bytes[offset + 0x0d] = (sector_count >> 8) & 0xff
-                self._bytes[offset + 0x0e] = (sector_count >> 16) & 0xff
-                self._bytes[offset + 0x0f] = (sector_count >> 24) & 0xff
+                self._pt[offset + 0x0c] = sector_count & 0xff
+                self._pt[offset + 0x0d] = (sector_count >> 8) & 0xff
+                self._pt[offset + 0x0e] = (sector_count >> 16) & 0xff
+                self._pt[offset + 0x0f] = (sector_count >> 24) & 0xff
 
                 next_start += sector_count
 
@@ -212,37 +219,72 @@ class gpt(_partition):
         self._ptes = bytearray(b'\0'*(128*128))
 
         # Binary representation
-        empty = bytearray(b"\0"*512)
+        self._ptpri = bytearray(b"\0"*MBR_SECTOR_SIZE)
         # "EFI PART" signature
-        empty[0x00] = 0x45 ; empty[0x01] = 0x46 ; empty[0x02] = 0x49 ; empty[0x03] = 0x20
-        empty[0x04] = 0x50 ; empty[0x05] = 0x41 ; empty[0x06] = 0x52 ; empty[0x07] = 0x54
+        self._ptpri[0x00] = 0x45 ; self._ptpri[0x01] = 0x46 ; self._ptpri[0x02] = 0x49 ; self._ptpri[0x03] = 0x20
+        self._ptpri[0x04] = 0x50 ; self._ptpri[0x05] = 0x41 ; self._ptpri[0x06] = 0x52 ; self._ptpri[0x07] = 0x54
         # Revision
-        empty[0x08] = 0x00 ; empty[0x09] = 0x00 ; empty[0x0a] = 0x01 ; empty[0x0b] = 0x00
+        self._ptpri[0x08] = 0x00 ; self._ptpri[0x09] = 0x00 ; self._ptpri[0x0a] = 0x01 ; self._ptpri[0x0b] = 0x00
         # Header size (LE)
-        empty[0x0c] = 0x5c ; empty[0x0d] = 0x00 ; empty[0x0e] = 0x00 ; empty[0x0f] = 0x00
+        self._ptpri[0x0c] = 0x5c ; self._ptpri[0x0d] = 0x00 ; self._ptpri[0x0e] = 0x00 ; self._ptpri[0x0f] = 0x00
         # LBA of this copy (1) (address of other copy generated later) (LE)
-        empty[0x18] = 0x01 ; empty[0x19] = 0x00 ; empty[0x1a] = 0x00 ; empty[0x1b] = 0x00
-        empty[0x1c] = 0x00 ; empty[0x1d] = 0x00 ; empty[0x1e] = 0x00 ; empty[0x1f] = 0x00
+        self._ptpri[0x18] = 0x01 ; self._ptpri[0x19] = 0x00 ; self._ptpri[0x1a] = 0x00 ; self._ptpri[0x1b] = 0x00
+        self._ptpri[0x1c] = 0x00 ; self._ptpri[0x1d] = 0x00 ; self._ptpri[0x1e] = 0x00 ; self._ptpri[0x1f] = 0x00
         # First usable LBA (2048) (LE)
-        empty[0x28] = 0x00 ; empty[0x29] = 0x08 ; empty[0x2a] = 0x00 ; empty[0x2b] = 0x00
-        empty[0x2c] = 0x00 ; empty[0x2d] = 0x00 ; empty[0x2e] = 0x00 ; empty[0x2f] = 0x00
+        self._ptpri[0x28] = 0x00 ; self._ptpri[0x29] = 0x08 ; self._ptpri[0x2a] = 0x00 ; self._ptpri[0x2b] = 0x00
+        self._ptpri[0x2c] = 0x00 ; self._ptpri[0x2d] = 0x00 ; self._ptpri[0x2e] = 0x00 ; self._ptpri[0x2f] = 0x00
         # Disk GUID
-        empty[0x38] = 0xde ; empty[0x39] = 0xad ; empty[0x3a] = 0xbe ; empty[0x3b] = 0xef
-        empty[0x3c] = 0xde ; empty[0x3d] = 0xad ; empty[0x3e] = 0xbe ; empty[0x3f] = 0xef
-        empty[0x40] = 0xde ; empty[0x41] = 0xad ; empty[0x42] = 0xbe ; empty[0x43] = 0xef
-        empty[0x44] = 0xde ; empty[0x45] = 0xad ; empty[0x46] = 0xbe ; empty[0x47] = 0xef
+        self._ptpri[0x38] = 0xde ; self._ptpri[0x39] = 0xad ; self._ptpri[0x3a] = 0xbe ; self._ptpri[0x3b] = 0xef
+        self._ptpri[0x3c] = 0xde ; self._ptpri[0x3d] = 0xad ; self._ptpri[0x3e] = 0xbe ; self._ptpri[0x3f] = 0xef
+        self._ptpri[0x40] = 0xde ; self._ptpri[0x41] = 0xad ; self._ptpri[0x42] = 0xbe ; self._ptpri[0x43] = 0xef
+        self._ptpri[0x44] = 0xde ; self._ptpri[0x45] = 0xad ; self._ptpri[0x46] = 0xbe ; self._ptpri[0x47] = 0xef
         # Starting LBA of PTE list (2 for primary copy) (LE)
-        empty[0x48] = 0x02 ; empty[0x49] = 0x00 ; empty[0x4a] = 0x00 ; empty[0x4b] = 0x00
+        self._ptpri[0x48] = 0x02 ; self._ptpri[0x49] = 0x00 ; self._ptpri[0x4a] = 0x00 ; self._ptpri[0x4b] = 0x00
         # Size of PTE (128) (LE)
-        empty[0x54] = 0x80 ; empty[0x55] = 0x00 ; empty[0x56] = 0x00 ; empty[0x57] = 0x00
+        self._ptpri[0x54] = 0x80 ; self._ptpri[0x55] = 0x00 ; self._ptpri[0x56] = 0x00 ; self._ptpri[0x57] = 0x00
 
-        initial_crc = crc32(empty[:0x5c])
-        empty[0x10] = initial_crc & 0xff
-        empty[0x11] = (initial_crc >> 8) & 0xff
-        empty[0x12] = (initial_crc >> 16) & 0xff
-        empty[0x13] = (initial_crc >> 24) & 0xff
+        self._updatePts()
 
-        return(empty)
+
+    def _updatePts(self):
+        """
+        Recalculate secondary header location in primary then make a copy of the primary gpt header
+        and update necessary fields.
+        """
+
+        # Update LBA of other copy in primary header (LE) (the final sector should contain the backup header)
+        secondaryLBAAddress = (self.diskSize() // MBR_SECTOR_SIZE) - 1
+        for byte in range(8):
+            self._ptpri[0x20+byte] = (secondaryLBAAddress >> (byte * 8)) & 0xff
+
+        # Blank CRC bytes ready to recalculate them
+        for byte in range(4):
+            self._ptpri[0x10+byte] = 0
+
+        # generate pte bytes
+        for pte in range(128):
+            pass
+
+        # generate pte crc32 and copy into header
+        pte_crt = crc32(self._ptes)
+        for byte in range(4):
+            self._ptpri[0x58+byte] = (pte_crt >> (byte * 8)) & 0xff
+
+        # Copy the primary header to the secondary
+        self._ptsec = self._ptpri[:]
+
+        # in the copy switch the location address around
+        for byte in range(8):
+            self._ptsec[0x18+byte], self._ptsec[0x20+byte] = self._ptsec[0x20+byte], self._ptsec[0x18+byte]
+
+        # calculate crcs and sub in
+        pri_crc = crc32(self._ptpri[:0x5c])
+        sec_crc = crc32(self._ptsec[:0x5c])
+        print(hex(pri_crc))
+        print(hex(sec_crc))
+#        for byte in range(4):
+#            self._ptpri[0x10+byte] = (pri_crc >> (byte * 8)) & 0xff
+#            self._ptsec[0x10+byte] = (sec_crc >> (byte * 8)) & 0xff
 
 
     def addPartition(self, index, sizemb, filesystem, bootable=False):
@@ -261,16 +303,22 @@ class gpt(_partition):
             # maximum size that can be represented by (2**32 - 1) sectors: ((2**32 - 1) * 512) / 1024
             protective_mbr.addPartition(1, 2147483647, 0xee)
         # Tickle the chs h value to 255 for parition 1
-        protective_mbr._bytes[446 + 0x01] = 255
+        protective_mbr._pt[446 + 0x01] = 255
 
         # Write the data
         protective_mbr.write(file)
 
         with open(file, "rb+") as fp:
-            fp.seek(len(protective_mbr._bytes))
-            fp.write(self._bytes)
+            fp.seek(len(protective_mbr._pt))
+            fp.write(self._ptpri)
+            fp.seek(self.diskSize()-MBR_SECTOR_SIZE)
+            fp.write(self._ptsec)
 
 
     def diskSize(self):
-        return(16*1048576)
-        raise Exception("not implemented")
+        # The 0-2047s = 1Mb, gpt copy at end of disk = 1Mb
+        size = 2
+        for (sizemb, filesystem) in self._partitions:
+            size += sizemb if sizemb else 0
+
+        return(size*1048576)
